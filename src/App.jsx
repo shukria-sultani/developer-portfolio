a99dfc9b668f33f40768bc07d8061a8214590558
Shukria <shukriasul5@gmail.com>
2025-08-16 16:39:13 +0430

    Added the  responsive Navbar component and applied styling to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import React from 'react';
+import Navbar from './Components/Navbar';
 
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-      
+      <Navbar />
     </>
   )
 }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+
+// Navbar icons
+import homeIcon from "../assets/images/home.png"; 
+import aboutIcon from "../assets/images/about.png";
+import projectsIcon from "../assets/images/project.png";
+import contactIcon from "../assets/images/contact.png";
+import menuIcon from "../assets/images/menu.png"; 
+
+export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [activeLink, setActiveLink] = useState("home"); 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+    setIsMenuOpen(false); 
+  };
+
+  return (
+    <nav>
+      <div className="navbar responsive-navbar">
+        <img 
+          src={menuIcon} 
+          className="menu-icon" 
+          alt="Menu Icon" 
+          onClick={toggleMenu} 
+        />
+        <ul className={isMenuOpen ? "active" : ""}>
+          <li>
+            <a 
+              href="#" 
+              className={activeLink === "home" ? "active" : ""} 
+              onClick={() => handleLinkClick("home")}
+            >
+              <img src={homeIcon} alt="Home Icon" width="30px" height="30px" /> Home
+            </a>
+          </li>
+          <li>
+            <a 
+              href="#" 
+              className={activeLink === "about" ? "active" : ""} 
+              onClick={() => handleLinkClick("about")}
+            >
+              <img src={aboutIcon} alt="About Icon" width="30px" height="30px" /> About
+            </a>
+          </li>
+          <li>
+            <a 
+              href="#" 
+              className={activeLink === "projects" ? "active" : ""} 
+              onClick={() => handleLinkClick("projects")}
+            >
+              <img src={projectsIcon} alt="Project Icon" width="30px" height="30px" /> Projects
+            </a>
+          </li>
+          <li>
+            <a 
+              href="#" 
+              className={activeLink === "contact" ? "active" : ""} 
+              onClick={() => handleLinkClick("contact")}
+            >
+              <img src={contactIcon} alt="Contact Icon" width="30px" height="30px" /> Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
