ef8c7bb64b7d2acc6da72c6b58dabdf3cad917b4
Shukria <shukriasul5@gmail.com>
2025-08-17 22:55:01 +0430

    Updated the about dummy info to real information.

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -8,8 +8,8 @@ export default function MyProfile() {
   const skillsSummary = "My core skills include JavaScript, React, Node.js, and CSS, with a strong focus on building responsive and accessible interfaces.";
   const funFactsSummary = "When I'm not coding, I enjoy playing chess and hiking.";
 
-  const skillsList = ["JavaScript", "React", "Node.js", "CSS", "SQL"];
-  const funFactsList = ["I am a competitive chess player", "I love hiking in national parks", "I am a big fan of sci-fi movies"];
+  const skillsList = ["JavaScript", "React", "HTML", "CSS", "Boostrap", "Python", "Java"];
+  const funFactsList = ["I like watching documentaries", "I love walking in the nature", "I am interested in photography"];
   const experienceList = ["Worked as a computer teacher at New Horizon, Bamyan Center |May 2023 - August 2024|", "Completed an intership in web developement at Webstack Academy, Remote |July 2024 - September 2024|"]
 
   return (
