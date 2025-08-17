9526c743e33b52fbc9b5da8777a796d9800c9d01
Shukria <shukriasul5@gmail.com>
2025-08-17 23:11:23 +0430

    Updated the about dummy data with real data.

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,12 +5,12 @@ import AboutSection from './AboutSection';
 
 export default function MyProfile() {
   const experienceSummary = "I have about 2 years of experience in IT field, including teaching computer programs and frontend development.";
-  const skillsSummary = "My core skills include JavaScript, React, Node.js, and CSS, with a strong focus on building responsive and accessible interfaces.";
-  const funFactsSummary = "When I'm not coding, I enjoy playing chess and hiking.";
+  const skillsSummary = "My core skills include JavaScript, React, CSS, HTML, Bootstrap with a strong focus on building responsive and accessible interfaces.";
+  const funFactsSummary = "When I'm not coding, I enjoy meeting friends and reading novels.";
 
   const skillsList = ["JavaScript", "React", "HTML", "CSS", "Boostrap", "Python", "Java"];
   const funFactsList = ["I like watching documentaries", "I love walking in the nature", "I am interested in photography"];
-  const experienceList = ["Worked as a computer teacher at New Horizon, Bamyan Center |May 2023 - August 2024|", "Completed an intership in web developement at Webstack Academy, Remote |July 2024 - September 2024|"]
+  const experienceList = ["Worked as a computer teacher at New Horizon, Bamyan Center |May 2023 - August 2024|", "Completed an internship in web developement at Webstack Academy, Remote |July 2024 - September 2024|"]
 
   return (
     <>
